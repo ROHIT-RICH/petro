22b1f8bf37b3af8ad0dc4448c92c7e74254d16e1
Rohit <rvaishya69@gmail.com>
2025-11-12 11:03:30 +0530

    w

--- src/routes/favorites.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { auth } from "../middleware/auth.js";
-import { getFavorites, toggleFavorite } from "../controllers/favorites.controller.js";
-const router = Router();
-router.use(auth(["buyer","admin"]));
-router.get("/", getFavorites);                 // GET /api/favorites
-router.post("/:productId/toggle", toggleFavorite); // POST /api/favorites/:productId/toggle
-export default router;
